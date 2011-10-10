946f7ee9b771e32e661bbd25acd69153acbfc601
Luke Amdor <luke.amdor@t8webware.com>
2011-10-10 15:19:15 -0500

    handling quoted strings

--- src/main/scala/FlumeSinkEventHandlerListener.scala
+++ src/main/scala/FlumeSinkEventHandlerListener.scala
@@ -9,9 +9,9 @@ import com.cloudera.flume.conf.{Context, FlumeBuilder}
 class FlumeSinkEventHandlerListener(sink: EventSink) extends Actor {
   def this() = this(FlumeSinkEventHandlerListener.configuredSink)
   def this(sinkFlumeSpec: String) = this(FlumeSinkEventHandlerListener.sinkFor(sinkFlumeSpec))
-  
+
   self.dispatcher = FlumeSinkEventHandlerListener.dispatcher
- 
+
   def receive = {
     case e: EventHandler.Event =>
       val event = EventHandlerFlumeEvent(e)
@@ -22,22 +22,26 @@ class FlumeSinkEventHandlerListener(sink: EventSink) extends Actor {
   override def preStart() {
     sink.open
   }
+
+  override def postStop() {
+    sink.close
+  }
 }
 
 object FlumeSinkEventHandlerListener {
 
   def listenerFor(sinkFlumeSpec: String): ActorRef = listenerFor(sinkFor(sinkFlumeSpec))
   def listenerFor(sink: EventSink): ActorRef = Actor.actorOf(new FlumeSinkEventHandlerListener(sink)).start
-    
+
   def addListenerSink(sinkFlumeSpec: String): Unit = addListenerSink(sinkFor(sinkFlumeSpec))
   def addListenerSink(sink: EventSink): Unit = EventHandler.addListener(listenerFor(sink))
 
   private[flume] def sinkFor(sinkFlumeSpec: String) = FlumeBuilder.buildSink(new Context, sinkFlumeSpec)
-  
-  private[flume] def configuredSink = sinkFor(config.getString("akka.flume-event-handler.sink", "console"))
-  
+
+  private[flume] def configuredSink = sinkFor(config.getString("akka.flume-event-handler.sink", "console").replaceAll("\\\"", "\""))
+
   lazy val dispatcher = Dispatchers.newExecutorBasedEventDrivenDispatcher("akka-event-flume-listener").build
-  
+
   implicit def str2Bytes(str: String): Array[Byte] = str.getBytes
 }
-  
+
